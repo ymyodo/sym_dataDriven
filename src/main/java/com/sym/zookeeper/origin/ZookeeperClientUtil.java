d84e7ac03558669ea133305e78d0b3dc3cf20b90
shenyanming <sym542569199@gmail.com>
2020-04-23 22:04:25 +0800

    zookeeper

--- src/main/java/com/sym/zookeeper/origin/ZookeeperClientUtil.java
+++ src/main/java/com/sym/zookeeper/origin/ZookeeperClientUtil.java
@@ -1,6 +1,5 @@
 package com.sym.zookeeper.origin;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.zookeeper.WatchedEvent;
@@ -11,8 +10,7 @@ import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -24,86 +22,134 @@ import java.util.concurrent.TimeoutException;
  */
 @Slf4j
 public class ZookeeperClientUtil {
-    public static void main(String[] args) {
-        String connectionString = "127.0.0.1:2181";
-        long connectionTimeout = 10000;
-        int sessionTimeout = 10000;
-        Watcher watcher = new Watcher() {
-            @Override
-            public void process(WatchedEvent event) {
-                //
-            }
-        };
-        boolean canBeReadOnly = false;
-        ZooKeeper zooKeeper = ZookeeperClientUtil.newZookeeper(connectionString, connectionTimeout, sessionTimeout, watcher, canBeReadOnly, -1, null);
-        System.out.println(zooKeeper);
+
+    /**
+     * 默认连接超时1分钟
+     */
+    private final static long DEFAULT_CONNECTION_TIMEOUT = 60 * 1000;
+
+    /**
+     * 默认心跳会话超时 15s
+     */
+    private final static int DEFAULT_SESSION_TIMEOUT = 15 * 1000;
+
+    public static ZooKeeper newZookeeper(String connectionString){
+        return newZookeeper(connectionString, DEFAULT_CONNECTION_TIMEOUT,
+                DEFAULT_SESSION_TIMEOUT, null, false,-1, null);
     }
 
-    /*
-     * 创建一个Zookeeper最全需要5个参数, 分别为：
-     * connectString, 表示zk集群地址, 例如：192.168.1.1:2181,192.16.1.2:2181,192.168.1.3:2181
-     * sessionTimeout, 表示与zk服务器的心跳超时时间, 单位毫秒
-     * watcher, 监听器
-     * canBeReadOnly, 表示此次会话是不是只读模式
-     * sessionId和sessionPasswd, 表示会话的id和会话密码, 用来进行会话复用
-     */
+    public static ZooKeeper newZookeeper(String connectionString, long connectionTimeout){
+        return newZookeeper(connectionString, connectionTimeout,
+                DEFAULT_SESSION_TIMEOUT, null, false, -1, null);
+    }
 
-    public static ZooKeeper newZookeeper(String connectionString, long connectionTimeout, int sessionTimeout, Watcher watcher,
-                                         boolean canBeReadOnly, long sessionId, String sessionPassword) {
-        CyclicBarrier cyclicBarrier = new CyclicBarrier(1);
+    public static ZooKeeper newZookeeper(String connectionString, Watcher watcher){
+        return newZookeeper(connectionString, DEFAULT_CONNECTION_TIMEOUT, DEFAULT_SESSION_TIMEOUT,
+                watcher, false, -1, null);
+    }
+
+    public static ZooKeeper newZookeeper(String connectionString,
+                                         long connectionTimeout,
+                                         int sessionTimeout,
+                                         Watcher watcher,
+                                         boolean canBeReadOnly,
+                                         long sessionId, String sessionPassword) {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
         ZooKeeper zookeeper;
         try {
+            Watcher watcherProxy = WatchProxy.getProxy(countDownLatch, watcher);
             if (sessionId > 0 && StringUtils.isNoneBlank(sessionPassword)) {
-                zookeeper = new ZooKeeper(connectionString, sessionTimeout, WatchProxy.getProxy(cyclicBarrier, watcher), sessionId, sessionPassword.getBytes(), canBeReadOnly);
+                /*
+                 * 创建一个Zookeeper最全需要5个参数, 分别为：
+                 * connectString, 表示zk集群地址, 例如：192.168.1.1:2181,192.16.1.2:2181,192.168.1.3:2181
+                 * sessionTimeout, 表示与zk服务器的心跳超时时间, 单位毫秒
+                 * watcher, 监听器
+                 * sessionId和sessionPasswd, 表示会话的id和会话密码, 可以用于复用会话
+                 * canBeReadOnly, 标志此次会话是不是只读模式
+                 */
+                zookeeper = new ZooKeeper(connectionString, sessionTimeout, watcherProxy, sessionId, sessionPassword.getBytes(), canBeReadOnly);
             } else {
-                zookeeper = new ZooKeeper(connectionString, sessionTimeout, WatchProxy.getProxy(cyclicBarrier, watcher), canBeReadOnly);
+                zookeeper = new ZooKeeper(connectionString, sessionTimeout, watcherProxy, canBeReadOnly);
             }
-            cyclicBarrier.await();
+            // countDownLatch.await(connectionTimeout, TimeUnit.MILLISECONDS);
+            countDownLatch.await();
+            if(zookeeper.getState() != ZooKeeper.States.CONNECTED){
+                throw new TimeoutException("连接zookeeper超时");
+            }
             return zookeeper;
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException | TimeoutException e) {
             log.error("创建zookeeper失败：{}", e.getMessage());
-        } catch (InterruptedException | BrokenBarrierException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-        return null;
     }
 
+    /**
+     * 监听器代理期
+     */
+    private static class WatchProxy {
 
-    private static class WatchProxy{
-
-        private static ClassLoader classLoader  = WatchProxy.class.getClassLoader();
+        private static ClassLoader classLoader = WatchProxy.class.getClassLoader();
         private static Class[] classes = new Class[]{Watcher.class};
+        /**
+         * 默认监听器
+         */
+        private static Watcher defaultWatcher = event -> {
+            // do nothing
+        };
 
-        public static Watcher getProxy(CyclicBarrier cyclicBarrier, Watcher watcher){
-            return (Watcher)Proxy.newProxyInstance(classLoader, classes, new WatcherHandler(cyclicBarrier, watcher));
-        }
-    }
-
-    @AllArgsConstructor
-    @Slf4j
-    private static class WatcherHandler implements InvocationHandler{
-
-        private CyclicBarrier cyclicBarrier;
-        private Watcher watcher;
-
-
-        @Override
-        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            boolean isCall = false;
-            if("process".equals(method.getName())){
-                try {
-                    WatchedEvent watchedEvent = (WatchedEvent)args[0];
-                    if(watchedEvent.getState() == Watcher.Event.KeeperState.SyncConnected){
-                        cyclicBarrier.await();
-                        isCall = true;
-                    }
-                }finally {
-                    if(!isCall){
-                        cyclicBarrier.await();
-                    }
-                }
+        /**
+         * 创建代理对象
+         * @param countDownLatch 同步工具
+         * @param watcher 监听器
+         */
+        public static Watcher getProxy(CountDownLatch countDownLatch, Watcher watcher) {
+            InvocationHandler invocationHandler;
+            if(null == watcher){
+                invocationHandler = new WatcherHandler(countDownLatch, defaultWatcher, true);
+            }else{
+                invocationHandler = new WatcherHandler(countDownLatch, watcher, false);
             }
-            return method.invoke(watcher, args);
+            return (Watcher) Proxy.newProxyInstance(classLoader, classes, invocationHandler);
+        }
+
+
+        /**
+         * 代理类处理逻辑
+         */
+        @Slf4j
+        private static class WatcherHandler implements InvocationHandler {
+
+            public WatcherHandler(CountDownLatch countDownLatch, Watcher watcher, boolean isIgnoreToInvoke){
+                this.countDownLatch = countDownLatch;
+                this.watcher = watcher;
+                this.isIgnoreToInvoke = isIgnoreToInvoke;
+                this.isInit = false;
+            }
+
+            private CountDownLatch countDownLatch;
+            private Watcher watcher;
+            private boolean isInit;
+            private boolean isIgnoreToInvoke;
+
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                if("process".equals(method.getName()) &&
+                        ((WatchedEvent)args[0]).getState() == Watcher.Event.KeeperState.SyncConnected){
+                    if(!isInit){
+                        countDownLatch.countDown();
+                        countDownLatch = null;
+                        isInit = true;
+                    }else{
+                        if(!isIgnoreToInvoke){
+                            return method.invoke(watcher, args);
+                        }
+                    }
+                }else{
+                    // Object类的其它方法走这里
+                    return method.invoke(watcher, args);
+                }
+                return null;
+            }
         }
     }
 }
--- src/main/java/com/sym/zookeeper/origin/ZookeeperTest.java
+++ src/main/java/com/sym/zookeeper/origin/ZookeeperTest.java
@@ -1,5 +1,13 @@
 package com.sym.zookeeper.origin;
 
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.ACL;
+import org.apache.zookeeper.data.Stat;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
 /**
  * 原生的zookeeper驱动
  *
@@ -7,4 +15,17 @@ package com.sym.zookeeper.origin;
  * Created on 2020/4/10 18:32
  */
 public class ZookeeperTest {
+
+    @Test
+    public void test0001() throws KeeperException, InterruptedException, IOException {
+        Watcher watcher = event -> {
+            System.out.println(event.getState());
+            System.out.println(event.getType());
+            System.out.println(event.getPath());
+            System.out.println(event.getWrapper());
+        };
+        ZooKeeper zooKeeper = ZookeeperClientUtil.newZookeeper("127.0.0.1:2181", watcher);
+        String s = zooKeeper.create("/test", "666".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+        System.out.println("创建节点：" + s);
+    }
 }
