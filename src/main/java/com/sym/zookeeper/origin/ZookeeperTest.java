5717f97e3f2fc2cf1795caf94d3cc885c9e1bd8c
shenym <542569199@qq.com>
2020-04-25 13:34:10 +0800

    zookeeper

--- src/main/java/com/sym/zookeeper/origin/ZookeeperTest.java
+++ src/main/java/com/sym/zookeeper/origin/ZookeeperTest.java
@@ -3,11 +3,16 @@ package com.sym.zookeeper.origin;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.ACL;
+import org.apache.zookeeper.data.Stat;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 原生的zookeeper驱动
@@ -18,13 +23,19 @@ import java.util.concurrent.CyclicBarrier;
 @Slf4j
 public class ZookeeperTest {
 
-    private String zookeeperHost = "127.0.0.1:2181";
     private ZooKeeper zooKeeper;
 
     @Before
     public void before() {
-        zooKeeper = ZookeeperClientUtil.newZookeeper(zookeeperHost);
+        String zookeeperHost = "127.0.0.1:2181";
+        Watcher defaultWatcher = event -> log.info("事件发生: {}", event);
+        zooKeeper = ZookeeperClientUtil.newZookeeper(zookeeperHost, defaultWatcher);
         log.info("zk客户端：{}", zookeeperHost);
+        try {
+            zooKeeper.delete("/test", -1);
+        }catch (Exception e){
+            log.error("ignore: {}", e.getMessage());
+        }
     }
 
     /**
@@ -130,5 +141,100 @@ public class ZookeeperTest {
             }
         }, ctx);
         cyclicBarrier.await();
+    }
+
+    /**
+     * 同步获取一个节点旗下的所有子节点
+     */
+    @Test
+    public void syncGetChildren() throws KeeperException, InterruptedException {
+        ArrayList<ACL> aclList = ZooDefs.Ids.OPEN_ACL_UNSAFE;
+
+        // 创建父节点
+        String path = "/test";
+        String info = zooKeeper.create(path, null, aclList, CreateMode.PERSISTENT);
+        log.info("创建父节点: {}", info);
+
+        // 创建子节点
+        String childPath0 = "/test/a1";
+        info = zooKeeper.create(childPath0, "first".getBytes(), aclList, CreateMode.EPHEMERAL);
+        log.info("创建子节点a1: {}", info);
+
+        // 创建子节点2
+        String childPath1 = "/test/a2";
+        info = zooKeeper.create(childPath1, "second".getBytes(), aclList, CreateMode.EPHEMERAL);
+        log.info("创建子节点a2: {}", info);
+
+        /*
+         * getChildren()有多个参数组合方式, 具体为：
+         * path, 获取此节点旗下的所有子节点;
+         * watcher, 此次获取完子节点后, 子节点列表发生变更后, zk服务端就会通过这个watcher向客户端发起通知, 此参数允许为null, 有个坑爹的东西就是这个监听器是一次性的, 一次回调后
+         *          就失效了, 需要重复注册
+         * watch, 表示是否需要注册一个watcher, 为false表示不需要注册监听器, 为true会使用创建 new Zookeeper() 时使用的那个watcher, 同上面一样这个也是回调一次也失效了;
+         * cb, 异步回调函数, 用在异步获取子节点的场景;
+         * ctx, 异步回调函数使用的附加参数;
+         * stat, 指定数据节点的状态信息, 此对象会在方法执行过程中被zk服务端响应的新stat替换掉
+         *
+         */
+        List<String> children = zooKeeper.getChildren(path, new Watcher() {
+
+            @Override
+            public void process(WatchedEvent event) {
+                log.info("事件发生: {}", event);
+                // 一次成功的回调事件后, 需要重新注册, 因为当前的这个watcher失效了
+                // zooKeeper.getChildren(path, new Watcher() {});
+            }
+        });
+        log.info("子节点路径：{}", children);
+
+        // 创建新的子节点
+        String childPath2 = "/test/a3";
+        info = zooKeeper.create(childPath2, "third".getBytes(), aclList, CreateMode.EPHEMERAL);
+        log.info("创建子节点a3: {}", info);
+
+        // 删除旧的子节点, 此时的节点变更就没有回调了, 因为watcher只生效一次
+        zooKeeper.delete(childPath1, -1);
+    }
+
+    /**
+     * 异步获取子节点列表
+     */
+    @Test
+    public void asyncGetChildren() throws KeeperException, InterruptedException, BrokenBarrierException {
+        ArrayList<ACL> aclList = ZooDefs.Ids.OPEN_ACL_UNSAFE;
+
+        // 创建父节点
+        String path = "/test";
+        String info = zooKeeper.create(path, null, aclList, CreateMode.PERSISTENT);
+        log.info("创建父节点: {}", info);
+
+        // 创建子节点
+        String childPath0 = "/test/a1";
+        info = zooKeeper.create(childPath0, "first".getBytes(), aclList, CreateMode.EPHEMERAL);
+        log.info("创建子节点a1: {}", info);
+
+        // 创建子节点2
+        String childPath1 = "/test/a2";
+        info = zooKeeper.create(childPath1, "second".getBytes(), aclList, CreateMode.EPHEMERAL);
+        log.info("创建子节点a2: {}", info);
+
+        //异步获取
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(2);
+        zooKeeper.getChildren(path, new Watcher() {
+            @Override
+            public void process(WatchedEvent event) {
+                log.info("子节点事件发生: {}", event);
+            }
+        }, new AsyncCallback.Children2Callback() {
+            @SneakyThrows
+            @Override
+            public void processResult(int rc, String path, Object ctx, List<String> children, Stat stat) {
+                log.info("异步回调: rc: {}, path: {}, ctx: {}, children: {}, stat: {}", rc, path, ctx, children, stat);
+                cyclicBarrier.await();
+            }
+        }, "模拟上下文对象");
+        cyclicBarrier.await();
+        // 删除一个子节点
+        zooKeeper.delete(childPath1, -1);
     }
 }
